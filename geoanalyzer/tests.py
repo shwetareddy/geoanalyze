129f1d96701b2de878dc301ce13055741be79640
Swetha Reddy <sreddy@fool.com>
2016-08-15 18:07:18 -0400

    add filter views

---
+++ geoanalyzer/tests.py
@@ -0,0 +1,10 @@
+import unittest
+from django.test import Client
+
+class PageTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
